d92391cc2f4c579d73d4774099eadea16b23a754
jatinsajwan3841 <jatinkumar@evolvfit.in>
2021-07-20 16:53:58 +0530

    prettier & routes

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-As mentioned in the Evolv Frontend Challenge, I tried all the 3 levels.
+Dish management having :
 
-The gh-pages link: https://jatinsajwan3841.github.io/evolvFchallenge/
+-   Login page : It's the home page. Login as per instruction on this page.
+-   Admin page : Can add, edit, delete dishes and it's ingredients.
+-   User page : Can view and search among available dishes.
+
+The gh-pages link: https://jatinsajwan3841.github.io/dish-management/
--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import Admin from "./components/admin";
 import User from "./components/user";
-import Login from "./components/login"
+import Login from "./components/login";
 import { Provider } from "react-redux";
 import store from "./store";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
-  return (
-    <Provider store={store}>
-    <Router>
-      <Switch>
-          <Route path="/evolvFchallenge/" exact component={Login} />
-          <Route path="/evolvFchallenge/admin" exact component={Admin} />
-          <Route path="/evolvFchallenge/user" exact component={User} />
-          <Route>
-            <button><Link to="/evolvFchallenge/">Go back to homepage</Link></button>
-            <img src='https://cdn.mos.cms.futurecdn.net/PuXipAW3AXUzUJ4uYyxPKC-1200-80.jpg' style={{maxWidth: '100%', height: 'auto'}}/>            
-            </Route>
-        </Switch>
-    </Router>
-    </Provider>
-  );
+    return (
+        <Provider store={store}>
+            <Router>
+                <Switch>
+                    <Route path="/dish-management/" exact component={Login} />
+                    <Route
+                        path="/dish-management/admin"
+                        exact
+                        component={Admin}
+                    />
+                    <Route
+                        path="/dish-management/user"
+                        exact
+                        component={User}
+                    />
+                    <Route>
+                        <button>
+                            <Link to="/dish-management/">
+                                Go back to homepage
+                            </Link>
+                        </button>
+                        <img
+                            src="https://cdn.mos.cms.futurecdn.net/PuXipAW3AXUzUJ4uYyxPKC-1200-80.jpg"
+                            style={{ maxWidth: "100%", height: "auto" }}
+                        />
+                    </Route>
+                </Switch>
+            </Router>
+        </Provider>
+    );
 }
 
 export default App;
